5ab96c13534ca3da6e69cce398f3cb14edae76b4
Mogle7Arkad <george7ali@gmail.com>
2023-06-05 23:31:51 +0100

    carried out the exercise. though i have to peep at intervals into the solution to scale through.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, dig){
+  if(dig < 0) return 'ERROR';
+  let string = "";
+  for(let i = 0; i < dig; i++){
+    string += word;
+  }
+  return string;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
+};
+console.log(0,0);
+
+const subtract = function subt(c,d) {
+	return c-d;
+};
+//console.log(3,2);
+
+const sum = function(array) {
+	return array.reduce((e, f) => e + f, 0)
 };
 
-const subtract = function() {
-	
+const multiply = function mult(array) {
+  return array.length
+  ?array.reduce((accumulator, nextIem)=> accumulator*nextIem)
+  :0
 };
 
-const sum = function() {
-	
+
+const power = function(m, n) {
+	return Math.pow(m, n);
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(p) {
+	if(p === 0) return 1;
+  let product = 1;
+  for (let i = p; i > 0; i--){
+    product *=i;
+  }
+  return product;
 };
 
 // Do not edit below this line
